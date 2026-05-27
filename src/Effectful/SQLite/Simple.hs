7a5249a81b0c7e72b79deaf70c68e669768bfef7
Diogo Castro <dc@diogocastro.com>
2026-05-27 22:20:23 +0100

    Added export list to `Simple` module

--- src/Effectful/SQLite/Simple.hs
+++ src/Effectful/SQLite/Simple.hs
@@ -1,6 +1,77 @@
 module Effectful.SQLite.Simple
-  ( SQLite,
+  ( -- * Effects
+    SQLite,
+    useConnection,
+
+    -- * Interpreters
+    runSQLiteUnsync,
+    runSQLiteSync,
+
+    -- * Connections
+    S.open,
+    S.close,
+    S.withConnection,
+    S.setTrace,
+
+    -- * Queries that return results
+    query,
+    query_,
+    queryWith,
+    queryWith_,
+    queryNamed,
+    lastInsertRowId,
+    changes,
+    totalChanges,
+
+    -- * Queries that stream results
+    fold,
+    fold_,
+    foldNamed,
+
+    -- * Statements that do not return results
+    execute,
+    execute_,
+    executeMany,
+    executeNamed,
+    S.field,
+
+    -- * Transactions
+    withTransaction,
+    withImmediateTransaction,
+    withExclusiveTransaction,
+    withSavepoint,
+
+    -- * Low-level statement API for stream access and prepared statements
+    openStatement,
+    closeStatement,
+    withStatement,
+    bind,
+    bindNamed,
+    reset,
+    columnName,
+    columnCount,
+    withBind,
+    nextRow,
+
+    -- ** Exceptions
+    S.FormatError (..),
+    S.ResultError (..),
+    S.SQLError (..),
+    S.Error (..),
+
+    -- * Types
+    S.Query (..),
+    S.Connection (..),
+    S.ToRow (..),
+    S.FromRow (..),
+    S.Only (..),
+    (S.:.) (..),
+    S.SQLData (..),
+    S.Statement (..),
+    S.ColumnIndex (..),
+    S.NamedParam (..),
   )
 where
 
+import Database.SQLite.Simple qualified as S
 import Effectful.SQLite.Simple.Internal
